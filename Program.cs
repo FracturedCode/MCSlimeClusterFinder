047c05d3546e486dc1a3f2179bac54160a4dcfca
Gabe <gabecarpenter@gmail.com>
2020-09-16 15:29:13 -0700

    Made new tests for c# implimentation

--- Program.cs
+++ Program.cs
@@ -195,11 +195,11 @@ namespace SlimeChunkDenseFinder
             }
             tParams.Complete = true;
         }
-        bool isSlimeChunk(int x, int z)
+        bool isSlimeChunk(int i, int j)
         {
             // Implementation of this from java:
             // new Random(seed + (long) (i * i * 4987142) + (long) (i * 5947611) + (long) (j * j) * 4392871L + (long) (j * 389711) ^ 987234911L).nextInt(10) == 0
-            long seed = ((_worldSeed + (x * x * 4987142) + (x * 5947611) + (z * z * 4392871) + (z * 389711 ^ 987234911L)) ^ 0x5DEECE66DL) & ((1L << 48) - 1);
+            long seed = ((_worldSeed + (long) (i * i * 4987142) + (long) (i * 5947611) + (long) (j * j) * 4392871L + (long) (j * 389711) ^ 987234911L) ^ 0x5DEECE66DL) & ((1L << 48) - 1);
             int bits, val;
             do
             {
@@ -223,6 +223,35 @@ namespace SlimeChunkDenseFinder
                     }
                 }
             }
+        }
+        void TestAgainstJavaRandomOutput()
+        {
+            string[] results = File.ReadAllLines("randomOutput.txt");
+            foreach (string[] r in results.Select(r => r.Split(',')))
+            {
+                int inputSeedr = int.Parse(r[0]);
+                long initSeedr = long.Parse(r[1]);
+                int nextIntr = int.Parse(r[2]);
+                long finalSeedr = long.Parse(r[3]);
+
+                long initSeed = (inputSeedr ^ 0x5DEECE66DL) & ((1L << 48) - 1);
+                (int nextInt, long finalSeed) = this.nextInt(initSeed);
+                if (initSeedr != initSeed || nextIntr != nextInt || finalSeedr != finalSeed)
+                {
+                    throw new Exception();
+                }
+            }
+        }
+        (int, long) nextInt(long seed)
+        {
+            int bits, val;
+            do
+            {
+                seed = (seed * 0x5DEECE66DL + 0xBL) & ((1L << 48) - 1);
+                bits = (int)((ulong)seed >> 17);
+                val = bits % 10;
+            } while (bits - val + 9 < 0);
+            return (val, seed);
         }
     }
 }
