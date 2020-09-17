343431b692106d1d2453ab46587fb12558337778
Gabe <gabecarpenter@gmail.com>
2020-09-16 22:14:24 -0700

    Added test from JavaUtilRandomTest that illustrates the psuedorandom generator from Java

--- MCSlimeClusterFinder/Program.cs
+++ MCSlimeClusterFinder/Program.cs
@@ -195,11 +195,11 @@ namespace MCSlimeClusterFinder
             }
             tParams.Complete = true;
         }
-        protected static bool isSlimeChunk(int i, int j)
+        protected static bool isSlimeChunk(int x, int z)
         {
             // Implementation of this from java:
             // new Random(seed + (long) (i * i * 4987142) + (long) (i * 5947611) + (long) (j * j) * 4392871L + (long) (j * 389711) ^ 987234911L).nextInt(10) == 0
-            long seed = ((_worldSeed + (long) (i * i * 4987142) + (long) (i * 5947611) + (long) (j * j) * 4392871L + (long) (j * 389711) ^ 987234911L) ^ 0x5DEECE66DL) & ((1L << 48) - 1);
+            long seed = ((_worldSeed + (long) (x * x * 4987142) + (long) (x * 5947611) + (long) (z * z) * 4392871L + (long) (z * 389711) ^ 987234911L) ^ 0x5DEECE66DL) & ((1L << 48) - 1);
             int bits, val;
             do
             {
@@ -209,32 +209,7 @@ namespace MCSlimeClusterFinder
             } while (bits - val + 9 < 0);
             return val == 0;
         }
-        void TestRandomImplementation()
-        {
-            ImportChunks();
-            for (int i = -12500; i<12500; i++)  // Went to 12500 instead of 12501 because I made that mistake in the java script.
-            {
-                for (int j = -12500; j<12500; j++)
-                {
-                    if (isSlimeChunk(i, j) && !_slimeChunks.Contains((i, j)))
-                    {
-                        throw new Exception("Test Failed");
-                    }
-                }
-            }
-        }
 
-        (int, long) nextInt(long seed)
-        {
-            int bits, val;
-            do
-            {
-                seed = (seed * 0x5DEECE66DL + 0xBL) & ((1L << 48) - 1);
-                bits = (int)((ulong)seed >> 17);
-                val = bits % 10;
-            } while (bits - val + 9 < 0);
-            return (val, seed);
-        }
         protected static TimeSpan Time(Action action, string actionName = null)
         {
             var sw = new Stopwatch();
