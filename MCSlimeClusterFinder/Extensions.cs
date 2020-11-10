5c22f24612e41261ba0d0331f65389b248989e22
FracturedCode <coding1729@gmail.com>
2020-11-09 22:36:28 -0700

    It didn't add extensions

---
+++ MCSlimeClusterFinder/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using OpenCL.NetCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MCSlimeClusterFinder
+{
+    public static class Extensions
+    {
+        public static InfoBuffer GetInfo(this Device device, DeviceInfo param)
+            => Cl.GetDeviceInfo(device, param, out ErrorCode error);
+        public static InfoBuffer GetInfo(this Platform platform, PlatformInfo param)
+            => Cl.GetPlatformInfo(platform, param, out ErrorCode error);
+    }
+}
